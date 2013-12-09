6045e2b9810d6db93a4126eeda72698d65a937f0
idungOL <nntaclino@NumlockPH-South-Mac-mini-4.local>
2013-12-09 11:37:35 +0800

    fixed footer

--- rogomi/include/footer.php
+++ rogomi/include/footer.php
@@ -5,43 +5,63 @@
 	</div>
 
 	<div id="navBot">
-		<p>
-		<?php 
-		if($title == 'Home'){
-			echo '&nbsp;Home&nbsp;';
-		} else {
-			echo '<a href="home.php">&nbsp;Home&nbsp;</a>';
-		}		
-		?> <span class="vertBar">|</span>  
+		<ul id="navlist">
+			<li>
+				<?php 
+				if($title == 'Inquire'){
+					echo '&nbsp;Contact&nbsp;';
+				} else {
+					echo '<a id="contactLi" href="inquire.php">&nbsp;Contact&nbsp;</a>';
+				}
+				?>
+			</li>
+			<li class="vertBar">|</li>
+			
+			<li>
+				<?php 
+				if($title == 'Blog'){
+					echo '&nbsp;Blog&nbsp;';
+				} else {
+					echo '<a id="blogLi" href="http://numlock.com.ph/blog/" target="_blank">&nbsp;Blog&nbsp;</a>';
+				}
+				?>
+			</li>
+			<li class="vertBar">|</li>
+			
+			<li>
+				<?php 
+				if($title == 'Apps'){
+					echo '&nbsp;Products&nbsp;';
+				} else {
+					echo '<a id="productLi" href="apps.php">&nbsp;Products&nbsp;</a>';
+				}
+				?>
+			</li>
+			<li class="vertBar">|</li> 
+			
+			<li>
+				<?php 
+				if($title == 'About'){
+					echo '&nbsp;About Rogomi&nbsp;';
+				} else {
+					echo '<a id="aboutLi" href="about.php">&nbsp;About Rogomi&nbsp;</a>';
+				}		
+				?>
+			</li>
+			<li class="vertBar">|</li>
 		
-		<?php 
-		if($title == 'About'){
-			echo '&nbsp;About Rogomi&nbsp;';
-		} else {
-			echo '<a href="about.php">&nbsp;About Rogomi&nbsp;</a>';
-		}		
-		?> <span class="vertBar">|</span>  
-		<?php 
-		if($title == 'Apps'){
-			echo '&nbsp;Products&nbsp;';
-		} else {
-			echo '<a href="apps.php">&nbsp;Products&nbsp;</a>';
-		}
-		?> <span class="vertBar">|</span>  
-		<?php 
-		if($title == 'Blog'){
-			echo '&nbsp;Blog&nbsp;';
-		} else {
-			echo '<a href="http://numlock.com.ph/blog/" target="_blank">&nbsp;Blog&nbsp;</a>';
-		}
-		?> <span class="vertBar">|</span> 
-		<?php 
-		if($title == 'Inquire'){
-			echo '&nbsp;Contact&nbsp;';
-		} else {
-			echo '<a href="inquire.php">&nbsp;Contact&nbsp;</a>';
-		}
-		?></p>
+			<li>
+				<?php 
+				if($title == 'Home'){
+					echo '&nbsp;Home&nbsp;';
+				} else {
+					echo '<a href="home.php">&nbsp;Home&nbsp;</a>';
+				}		
+				?> 
+			</li>
+		
+			
+
 	</div>
 </div>
 
